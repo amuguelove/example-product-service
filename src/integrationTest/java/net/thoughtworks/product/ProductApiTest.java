bf95898a2f27fc98b46b573606f0a0cc488a0e8e
liangbo <908325713@qq.com>
2020-03-25 13:21:54 +0800

    remove docker compose tests

--- src/integrationTest/java/net/thoughtworks/product/ProductApiTest.java
+++ src/integrationTest/java/net/thoughtworks/product/ProductApiTest.java
@@ -1,17 +1,10 @@
 package net.thoughtworks.product;
 
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.DatabaseTearDown;
-import net.thoughtworks.utils.IntgTestBase;
-import org.junit.Test;
+//@DatabaseSetup(value = "classpath:/integration/product.yml")
+//@DatabaseTearDown
+public class ProductApiTest {
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasSize;
-
-@DatabaseSetup(value = "classpath:/integration/product.yml")
-@DatabaseTearDown
-public class ProductApiTest extends IntgTestBase {
-
+    /*
     @Test
     public void shouldGetAllProducts() {
         given()
@@ -21,5 +14,5 @@ public class ProductApiTest extends IntgTestBase {
                 .statusCode(200)
                 .body("$", hasSize(2))
         ;
-    }
+    }*/
 }
